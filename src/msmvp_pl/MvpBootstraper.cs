2e71174ebc9062b59d06f6fa08218285c997d683
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2012-02-21 21:42:13 +0100

    added custom bootstraper to enable serving static content from ~/assets folder

---
+++ src/msmvp_pl/MvpBootstraper.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using Nancy.Conventions;
+
+namespace msmvp_pl
+{
+    public class MvpBootstraper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+
+            conventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("assets")
+            );
+        }
+    }
+}
