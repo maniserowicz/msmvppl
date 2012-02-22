2b15e9ac71666646d430cfcf0ba51a2aa544d4b9
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2012-02-22 23:42:57 +0100

    some more info on mvp list page from db

--- src/msmvp_pl/Modules/MvpModule.cs
+++ src/msmvp_pl/Modules/MvpModule.cs
@@ -1,8 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
 using Nancy;
 using msmvp_pl.Core;
 
 namespace msmvp_pl.Modules
 {
+    public class MvpListModel
+    {
+        private readonly IEnumerable<dynamic> _mvps;
+
+        public MvpListModel(dynamic mvps)
+        {
+            _mvps = mvps.ToList();
+        }
+
+        public IEnumerable<dynamic> CurrentMvps()
+        {
+            return _mvps.Where(
+                x => ((IEnumerable<dynamic>) x.Nominations.ToList())
+                    .Any(y => y.EndDate == null)
+            );
+        }
+
+        public IEnumerable<dynamic> ExMvps()
+        {
+            return _mvps.Except(CurrentMvps());
+        }
+    }
+
     public class MvpModule : NancyModule
     {
         public MvpModule(IDbProvider dbProvider) : base("/mvp")
@@ -10,8 +35,8 @@ namespace msmvp_pl.Modules
             Get["/"] = _ =>
                 {
                     var db = dbProvider.GetDb();
-                    var mvps = db.mvps.WithNominations();
-                    return View["mvp-list", mvps];
+                    var mvps = db.mvps.All();
+                    return View["mvp-list", new MvpListModel(mvps)];
                 };
         }
     }
