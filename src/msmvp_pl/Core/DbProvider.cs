0cbee50fbb1459933facddeeefba38418238e10c
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2012-02-22 20:51:13 +0100

    using db in mvp-list.cshml view

---
+++ src/msmvp_pl/Core/DbProvider.cs
@@ -0,0 +1,17 @@
+﻿using Simple.Data;
+
+namespace msmvp_pl.Core
+{
+    public interface IDbProvider
+    {
+        dynamic GetDb();
+    }
+
+    public class SqlCeDbProvider : IDbProvider
+    {
+        public dynamic GetDb()
+        {
+            return Database.OpenNamedConnection("msmvp_pl");
+        }
+    }
+}
--- src/msmvp_pl/Modules/MvpModule.cs
+++ src/msmvp_pl/Modules/MvpModule.cs
@@ -1,12 +1,18 @@
 using Nancy;
+using msmvp_pl.Core;
 
 namespace msmvp_pl.Modules
 {
     public class MvpModule : NancyModule
     {
-        public MvpModule() : base("/mvp")
+        public MvpModule(IDbProvider dbProvider) : base("/mvp")
         {
-            Get["/"] = _ => View["mvp-list"];
+            Get["/"] = _ =>
+                {
+                    var db = dbProvider.GetDb();
+                    var mvps = db.mvps.WithNominations();
+                    return View["mvp-list", mvps];
+                };
         }
     }
 }
