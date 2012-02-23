6207ce23aec177a0a5b13f1024e9a496c2c20559
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2012-02-23 20:23:56 +0100

    rendering list of current and ex MVPs using simple.data eager fetching

--- src/msmvp_pl/Modules/MvpModule.cs
+++ src/msmvp_pl/Modules/MvpModule.cs
@@ -1,43 +1,48 @@
 using System.Collections.Generic;
-using System.Linq;
 using Nancy;
 using msmvp_pl.Core;
+using System.Linq;
 
 namespace msmvp_pl.Modules
 {
-    public class MvpListModel
-    {
-        private readonly IEnumerable<dynamic> _mvps;
-
-        public MvpListModel(dynamic mvps)
-        {
-            _mvps = mvps.ToList();
-        }
-
-        public IEnumerable<dynamic> CurrentMvps()
-        {
-            return _mvps.Where(
-                x => ((IEnumerable<dynamic>) x.Nominations)
-                    .Any(y => y.EndDate == null)
-            );
-        }
-
-        public IEnumerable<dynamic> ExMvps()
-        {
-            return _mvps.Except(CurrentMvps());
-        }
-    }
-
     public class MvpModule : NancyModule
     {
-        public MvpModule(IDbProvider dbProvider) : base("/mvp")
+        public MvpModule(IDbProvider dbProvider)
+            : base("/mvp")
         {
             Get["/"] = _ =>
                 {
-                    var db = dbProvider.GetDb();
-                    var mvps = db.mvps.WithNominations();
+                    var mvps = dbProvider.GetDb().mvps.WithNominations();
+
                     return View["mvp-list", new MvpListModel(mvps)];
                 };
+        }
+
+        public class MvpListModel
+        {
+            private readonly IEnumerable<dynamic> _mvps;
+
+            public MvpListModel(dynamic mvps)
+            {
+                _mvps = mvps;
+            }
+
+            private bool IsCurrent(dynamic mvp)
+            {
+                IEnumerable<dynamic> nominations = mvp.Nominations;
+                
+                return nominations.Any(y => y.EndDate == null);
+            }
+
+            public IEnumerable<dynamic> Current
+            {
+                get { return _mvps.Where(x => IsCurrent(x)); }
+            }
+
+            public IEnumerable<dynamic> Ex
+            {
+                get { return _mvps.Where(x => !IsCurrent(x)); }
+            }
         }
     }
 }
