1196e43131cf86ea34edc1b20dcc272fb711c311
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2012-02-25 08:36:04 +0100

    added DynamicData app

---
+++ src/DataEntryApp/DynamicData/FieldTemplates/Text.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" CodeBehind="Text.ascx.cs" Inherits="DataEntryApp.TextField" %>
+
+<asp:Literal runat="server" ID="Literal1" Text="<%# FieldValueString %>" />
+
---
+++ src/DataEntryApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataEntryApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DataEntryApp")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a8d37f0a-0aa5-490a-9f3a-d82a2d05550e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
