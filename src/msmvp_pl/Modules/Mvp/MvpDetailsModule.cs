5ced2cd983c138cf41f14d540691ce84578ac3ce
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2012-03-01 18:45:41 +0100

    content title can now have any characters - works with e.g. ( and )

--- src/msmvp_pl/Modules/Mvp/MvpDetailsModule.cs
+++ src/msmvp_pl/Modules/Mvp/MvpDetailsModule.cs
@@ -1,5 +1,7 @@
+using System;
 using Nancy;
 using msmvp_pl.Core;
+using System.Linq;
 
 namespace msmvp_pl.Modules.Mvp
 {
@@ -8,22 +10,26 @@ namespace msmvp_pl.Modules.Mvp
         public MvpDetailsModule(IDbProvider dbProvider)
         {
             Get["/{slug}"] = p =>
+            {
+                var mvp = dbProvider.GetDb()
+                    .mvps.FindAllBySlug(p["slug"])
+                    .WithContents()
+                    .WithLinks()
+                    .WithNominations()
+                    .FirstOrDefault()
+                    ;
+
+                mvp.StringSlug = new Func<string, string>(
+                    title => new string(title.Where(x => char.IsLetterOrDigit(x)).ToArray())
+                );
+
+                if (mvp == null)
                 {
-                    var mvp = dbProvider.GetDb()
-                        .mvps.FindAllBySlug(p["slug"])
-                        .WithContents()
-                        .WithLinks()
-                        .WithNominations()
-                        .FirstOrDefault()
-                        ;
+                    return HttpStatusCode.NotFound;
+                }
 
-                    if (mvp == null)
-                    {
-                        return HttpStatusCode.NotFound;
-                    }
-
-                    return View["mvp-details", mvp];
-                };
+                return View["mvp-details", mvp];
+            };
         }
     }
 }
